3764189cd0335fb538f5299ed16d040702fc75f2
shivamgondSuj <136161872+shivamgond420@users.noreply.github.com>
2024-06-09 04:00:45 -0700

    fix a lot of errors fix tree id

--- src/app/Featuers/Resipe/TreeAPI.ts
+++ src/app/Featuers/Resipe/TreeAPI.ts
@@ -1,12 +1,12 @@
-import axios from "axios";
-import { TreeInfo } from "../../../../type";
+// import axios from "axios";
+// import { TreeInfo } from "../../../../type";
 
-export async function Plantsdetails(id: string): Promise<TreeInfo | null> {
-  return new Promise<TreeInfo>((resolve, reject) => {
-    try {
-      const Data = axios.get(
-        "http://localhost:3000/api/Tree/TreeDetails/?id=" + id
-      );
-    } catch (error) {}
-  });
-}
+// export async function Plantsdetails(id: string): Promise<TreeInfo | null> {
+//   return new Promise<TreeInfo>((resolve, reject) => {
+//     try {
+//       const Data = axios.get(
+//         "http://localhost:3000/api/Tree/TreeDetails/?id=" + id
+//       );
+//     } catch (error) {}
+//   });
+// }
