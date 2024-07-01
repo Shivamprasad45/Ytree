093abda5ece839a3eeac9c3dd0a8cb972105667a
shivamgondSuj <136161872+shivamgond420@users.noreply.github.com>
2024-07-01 04:41:07 -0700

    Fix error Key Id is Delete

--- src/app/Tree/Ai/page.tsx
+++
@@ -1,8 +0,0 @@
-// "use server";
-// import { cookies } from "next/headers";
-
-// export default async function Ai() {
-//   const cookieStore = cookies();
-//   const id = cookieStore.get("id");
-//   return id;
-// }
