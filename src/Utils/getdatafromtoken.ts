c228e6c22891220cc8591ff0166f873bbf53b648
shivamgondSuj <136161872+shivamgond420@users.noreply.github.com>
2024-07-01 20:31:51 -0700

    Fix error .env set production

--- src/Utils/getdatafromtoken.ts
+++ src/Utils/getdatafromtoken.ts
@@ -1,11 +1,14 @@
 "use server";
 import jsonwebtoken from "jsonwebtoken";
 import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export default async function getdatafromtoken(req: NextRequest) {
   try {
-    const token = req.cookies.get("token")?.value!;
-    console.log("Token from cookies:", token);
+    const cookieStore = cookies();
+    const token = cookieStore.get("token")?.value || "";
+
+    console.log(token, "token");
 
     if (!token) {
       throw new Error("Token not found");
@@ -13,7 +16,7 @@ export default async function getdatafromtoken(req: NextRequest) {
 
     const decodedToken: any = jsonwebtoken.verify(
       token,
-      process.env.SECRET_KEY || "defaultSecretKey"
+      process.env.SECRET_KEY!
     );
 
     return decodedToken.id!;
