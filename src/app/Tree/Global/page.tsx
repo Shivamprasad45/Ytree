0171d3f18ad35678a62057d26b0c229e3bf47332
Shivam gond <codewithharry35434@gmail.com>
2024-09-07 07:57:28 +0530

    Gloable tree

--- src/app/Tree/Global/page.tsx
+++ src/app/Tree/Global/page.tsx
@@ -1,10 +1,14 @@
-import MapComponent from "@/app/Components/Map_components";
 import dynamic from "next/dynamic";
 import React from "react";
 
 // Dynamically import MapComponent
+const Map_components = dynamic(
+  () => import("@/app/Components/Map_components"),
+  {
+    ssr: false, // Disable server-side rendering if needed
+  }
+);
 
-const Map_components = dynamic(import("@/app/Components/Map_components"));
 const page = () => {
   return (
     <div>
