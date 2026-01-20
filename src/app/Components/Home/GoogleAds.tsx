63c3877c9d2e0a5e74171b7b216a96efe8d32c0b
Shivamprasad45 <codewithharry35434@gmail.com>
2026-01-20 20:51:05 +0530

    id change

--- src/app/Components/Home/GoogleAds.tsx
+++ src/app/Components/Home/GoogleAds.tsx
@@ -24,16 +24,12 @@ export default function GoogleAds() {
 
     return (
         <div className="w-full flex justify-center items-center my-8 overflow-hidden min-h-[100px] bg-gray-50/50 rounded-lg">
-            {/* 
-        REPLACE 'data-ad-slot' VALUE BELOW WITH YOUR ACTUAL AD UNIT ID FROM GOOGLE ADSENSE 
-        currently set to a placeholder '1234567890'
-      */}
             <ins
                 ref={adRef}
                 className="adsbygoogle"
                 style={{ display: "block", width: "100%" }}
                 data-ad-client="ca-pub-7763744084891101"
-                data-ad-slot="1234567890"
+                data-ad-slot="7168838066"
                 data-ad-format="auto"
                 data-full-width-responsive="true"
             />
