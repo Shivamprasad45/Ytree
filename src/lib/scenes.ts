90d8452d53267ffa190d8cf7fede66babcc2b02b
Shivamprasad45 <codewithharry35434@gmail.com>
2026-02-08 16:53:50 +0530

    use cdn

--- src/lib/scenes.ts
+++ src/lib/scenes.ts
@@ -2,27 +2,27 @@
 export const scenes = [
     {
         id: 0,
-        video: "/story/A_healthy_green_202602080953_521i6.mp4",
+        video: "https://res.cloudinary.com/dn633knvv/video/upload/q_auto,f_auto/v1770549373/A_healthy_green_202602080953_521i6_taboxa.mp4",
         text: "Once, Earth was alive."
     },
     {
         id: 1,
-        video: "/story/A_once_green_202602080955_rqb3w.mp4",
+        video: "https://res.cloudinary.com/dn633knvv/video/upload/q_auto,f_auto/v1770549361/A_once_green_202602080955_rqb3w_flneih.mp4",
         text: "We filled the air with poison."
     },
     {
         id: 2,
-        video: "/story/Dark_storm_clouds_202602081001_vqwmc.mp4",
+        video: "https://res.cloudinary.com/dn633knvv/video/upload/q_auto,f_auto/v1770549381/Dark_storm_clouds_202602081001_vqwmc_yckpef.mp4",
         text: "Even the rain began to hurt."
     },
     {
         id: 3,
-        video: "/story/A_forest_being_202602081006_82hdc.mp4",
+        video: "https://res.cloudinary.com/dn633knvv/video/upload/q_auto,f_auto/v1770549369/A_forest_being_202602081006_82hdc_pqqky3.mp4",
         text: "We took more than we gave back."
     },
     {
         id: 4,
-        video: "/story/A_cracked_and_202602081027_2flfi.mp4",
+        video: "https://res.cloudinary.com/dn633knvv/video/upload/q_auto,f_auto/v1770549370/A_cracked_and_202602081027_2flfi_dyycyw.mp4",
         text: "Earth is struggling to breathe."
     },
 ];
